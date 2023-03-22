1c96ca3528c5eca90b0474f2104bb38ca0718829
Rob <123372510+pezagna@users.noreply.github.com>
2023-03-22 19:40:59 -0300

    Atualizando Tabelas

--- Arquivo SQL/PROJETO INDIVIDUAL 1 - MÓDULO 3 - RESILIDATA.sql
+++ Arquivo SQL/PROJETO INDIVIDUAL 1 - MÓDULO 3 - RESILIDATA.sql
@@ -19,8 +19,8 @@ CREATE TABLE tecnologia (
   area VARCHAR(20)
 );
 
-CREATE TABLE tecnologia_empresa (
-  id_tecnologia_empresa INT PRIMARY KEY,
+CREATE TABLE utiliza (
+  id_utiliza INT PRIMARY KEY,
   id_empresa INT,
   id_tecnologia INT,
   FOREIGN KEY (id_empresa) REFERENCES empresa_parceira(id_empresa),
@@ -35,22 +35,9 @@ INSERT INTO tecnologia (id_tecnologia, nome_tecnologia, area)
 VALUES (1, 'Java', 'webdev'),
        (2, 'Python', 'dados');
 
-INSERT INTO tecnologia_empresa (id_tecnologia_empresa, id_empresa, id_tecnologia)
+INSERT INTO utiliza (id_utiliza, id_empresa, id_tecnologia)
 VALUES (1, 1, 1),
        (2, 1, 1);
-SELECT e.nome_empresa, t.nome_tecnologia, t.area
-FROM empresa_parceira e
-JOIN tecnologia_empresa te ON e.id_empresa = te.id_empresa
-JOIN tecnologia t ON te.id_tecnologia = t.id_tecnologia;
 
-SELECT t.nome_tecnologia, COUNT(*) AS qtd_empresas
-FROM tecnologia t
-JOIN tecnologia_empresa te ON t.id_tecnologia = te.id_tecnologia
-GROUP BY t.nome_tecnologia
-ORDER BY qtd_empresas DESC;
-
-SELECT e.nome_empresa, t.nome_tecnologia
-FROM empresa_parceira e
-JOIN tecnologia_empresa te ON e.id_empresa = te.id_empresa
-JOIN tecnologia t ON te.id_tecnologia = t.id_tecnologia
-WHERE t.area = 'webdev';
+select * from tecnologia
+select * FROM empresa_parceira
